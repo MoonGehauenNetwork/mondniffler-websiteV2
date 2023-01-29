be88b47206a4d4d5275312f5fc6134954de86776
dercrafter168 <mike@der-crafter.de>
2023-01-29 23:12:25 +0100

    21361456

---
+++ data/js/info.js
@@ -0,0 +1,84 @@
+var navbar =
+'<div class="dropdown">' +
+'<button class="dropbtn"><a href="./index.html"> Home </a></button>' +
+'</div> | ' +
+//Button 1 Finish
+'<div class="dropdown">' +
+'<button class="dropbtn"><a href="#"> Bibliothek </a></button>' +
+'<div class="dropdown-content">' +
+'<a href="#">Tropfender Kessel</a>' +
+'<a href="#">Godric Olivanders</a>' +
+'<a href="#">Weasleys Zauberhafte Zauber Scherze</a>' +
+'</div>' +
+'</div> | ' +
+//Button 2 Finish
+'<div class="dropdown-va">' +
+'<button class="dropbtn"><a href="#"> DCNG & Nifflers </a></button>' +
+'<div class="dropdown-content-va">' +
+'<a href="#">Modpack</a>' +
+'<a href="#">Texpack</a>' +
+'<a href="#">Shader</a>' +
+'</div>' +
+'</div> | '+
+'<div class="dropdown">' +
+'<button class="dropbtn"><a href="#"> Eulenpost </a></button>' +
+'</div> | ' +
+//Button 2 Finish
+'<div class="dropdown">' +
+'<button class="dropbtn"><a href="#"> Große Halle </a></button>' +
+'<div class="dropdown-content">' +
+'<a href="#">Twitter - Mondniffler</a>' +
+'<a href="#">Twitter - Team</a>' +
+'<a href="#">Instagram</a>' +
+'<a href="#">Discord</a>' +
+'<a href="#">TikTok</a>' +
+'<a href="#">Youtube</a>' +
+'</div>' +
+'</div> | ' +
+//Button 2 Finish
+'<div class="dropdown">' +
+'<button class="dropbtn"><a href="#"> Niffler Prohpet </a></button>' +
+'</div> | ' +
+//Button 2 Finish
+'<div class="dropdown-va">' +
+'<button class="dropbtn"><a href="#"> Verbotene Abteilung </a></button>' +
+'<div class="dropdown-content-va">' +
+'<a href="#">Sarah ist toll <3</a>' +
+'<a href="#">Lara ist toll</a>' +
+'<a href="#">Luki du Vogel</a>' +
+'<a href="#">Tötet was fliegt, Eulen jagt Verein e.V</a>' +
+'<a href="#">Tötet was trötet, Elefanten jagt Verein e.V</a>' +
+'</div>' +
+'</div>' ;
+//Button 2 Finish
+
+var fuß =
+'<div id="fussbereich">' +
+'<footer>© 2022 Copyright MGN | All Rights Reserved | <a href="https://contact.mgn.partner.dercrafter.de" target="_blank">Eulenpost</a> | <a href="https://privacy.mondniffler.de" target="_blank">Datenschutzerklärung</a> | <a href="https://impressum.mondniffler.de" target="_blank">Impressum</a></footer>' +
+'</div>' ;
+//Button 2 Finish
+
+var dynbg =
+'<video id="background-video" autoplay loop muted poster="../imgages/bg.jpg"> <!--Background VIdeo-->' +
+'<!--Background Video - src-->' +
+'<source src="./data/video/galaxy/bg.mp4" type="video/mp4">' +
+'<source src="./data/video/galaxy/bg.3gp" type="video/3gp">' +
+'<source src="./data/video/galaxy/bg.avi" type="video/avi">' +
+'<source src="./data/video/galaxy/bg.flv" type="video/flv">' +
+'<source src="./data/video/galaxy/bg.mkv" type="video/mkv">' +
+'<source src="./data/video/galaxy/bg.mov" type="video/mov">' +
+'<source src="./data/video/galaxy/bg.mpg" type="video/mpg">' +
+'<source src="./data/video/galaxy/bg.webm" type="video/webm">' +
+'<source src="./data/video/galaxy/bg.wmv" type="video/wmv">' +
+'<source src="./data/video/galaxy/bg.3g2" type="video/3g2">' +
+'<source src="./data/video/galaxy/bg.asf" type="video/asf">' +
+'<source src="./data/video/galaxy/bg.f4v" type="video/f4v">' +
+'<source src="./data/video/galaxy/bg.m2ts" type="video/m2ts">' +
+'<source src="./data/video/galaxy/bg.m2v" type="video/m2v">' +
+'<source src="./data/video/galaxy/bg.mts" type="video/mts">' +
+'<source src="./data/video/galaxy/bg.mxf" type="video/mxf">' +
+'<source src="./data/video/galaxy/bg.rm" type="video/rm">' +
+'<source src="./data/video/galaxy/bg.rmvb" type="video/rmvb">' +
+'<source src="./data/video/galaxy/bg.wtv" type="video/wtv">' +
+'<!--Background VIdeo - src-->' +
+'</video>' ;
